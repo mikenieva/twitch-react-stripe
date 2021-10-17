ebe21ede0c40e7d6e12e08ec47fd5cc7c1c51aac
Mike Nieva <m@mikenieva.com>
2021-10-16 21:40:35 -0500

    Realizamos instalación de react-router-dom y limpiamos App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // FRAGMENTOS
+    /**
+     * EN REACT EXISTE UNA REGLA DE QUE UN COMPONENTE DEBE DE TENER UNA ÚNICA ETIQUETA DOMINANTE / QUE ENGLOBA TODO
+     */
+    // FORMATO JSX
+    <>
+      <div>hola</div> 
+    </>
+    
   );
 }
 
